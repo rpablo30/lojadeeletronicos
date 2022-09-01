a3ed430cb31e772682b2bfd8696bdbfc0c72d4bc
rpablo30 <rpablo30@hotmail.com>
2022-09-01 18:31:33 -0300

    Projeto da loja de eletrônicos com o backend

---
+++ backend/src/main/java/com/lojao/eletronicos/lojaeletronicos/LojaeletronicosApplication.java
@@ -0,0 +1,15 @@
+package com.lojao.eletronicos.lojaeletronicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaeletronicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaeletronicosApplication.class, args);
+		System.out.println("Rodando..");
+
+	}
+
+}
---
+++ backend/src/test/java/com/lojao/eletronicos/lojaeletronicos/LojaeletronicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojao.eletronicos.lojaeletronicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaeletronicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
